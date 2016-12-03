071dcf5b82a158bbb1b21d528829dbb7a34f26c5
robwayne <robwaynegordon@gmail.com>
2016-12-03 14:57:57 +0400

    Added an images folder. Added color_wheel.png and implemented changing of color of ball.

--- ColorDitch.py
+++ ColorDitch.py
@@ -19,14 +19,19 @@ START_POS = (surface.get_width()//2, surface.get_height()-20)
 class ColorWheel(Sprite):
     def __init__(self, pos, radius):
         super().__init__()
-        self.image = pygame.Surface((radius, radius))
-        self.rect = self.image.get_rect()   
         self.pos = pos
         self.colors = {'red': [255,0,0],'green': [0,255,0], 'blue': [0,0,255], 'yellow': [0,255,255]} 
+        self.image = pygame.image.load('images/color_wheel.png').convert()
+        self.image = pygame.transform.smoothscale(self.image, (20,20)) #reduce image size
+        self.rect = pygame.Rect(pos, (radius,radius))
         
         
     def assignColor(self):
         return self.colors[['red','green','blue','yellow'][random.randrange(4)]]
+        
+    def display(self, surface):
+        pygame.draw.rect(surface, (255,255,255), self.rect)
+        surface.blit(self.image, self.pos)
         
 
 class ColorBall(Sprite):
@@ -37,22 +42,26 @@ class ColorBall(Sprite):
         self.pos = START_POS
         self.image = pygame.Surface((radius, radius))
         self.image.fill(self.color)
-        self.rect = self.image.get_rect()
+        self.rect = pygame.Rect(self.pos, (radius, radius))
         
         
     def display(self, surface):
+        pygame.draw.rect(surface, (255,255,255), self.rect)
         pygame.draw.circle(surface, self.color, self.pos, self.image.get_width())
         
     def move(self, direction):
         if direction.lower() == "up":
                 self.pos = (self.pos[0],self.pos[1]-10)
+                self.rect.move_ip(0,-10)
+                print("Up: ", self.rect)
         if self.pos != START_POS:
             if direction.lower() == "down":
                 self.pos = (self.pos[0],self.pos[1]+1)
+                self.rect.move_ip(0,1)
+                print("Down: ", self.rect)
                 
-    def updateColor(self, color):
+    def changeColor(self, color):
         self.color = color
- 
         
 ball = ColorBall(5)
 gravity = False
@@ -69,8 +78,14 @@ while True:
         
     if gravity:
         ball.move("down")
+        
+    colorwheel = ColorWheel((surface.get_width()//2-5,surface.get_height()//2), 10) 
+    if ball.rect.colliderect(colorwheel.rect):
+        print("collides")
+        ball.changeColor(colorwheel.assignColor())
     
     ball.display(surface)
+    colorwheel.display(surface)
     pygame.display.update()
 pygame.display.quit()
 
